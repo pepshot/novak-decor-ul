00d2146d05c29e46a80ae3cf4f8647984a6efe73
Alexander <118464247+pepshot@users.noreply.github.com>
2023-08-16 21:44:02 +0400

    добавления новой страницы - каталога

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, url_for
 from data import db_session
 
@@ -9,7 +11,15 @@ app.config['SECRET_KEY'] = 'first_project_by_KA'
 @app.route('/main', methods=['GET'])
 def main():
     css = url_for('static', filename='css/all.css')
-    return render_template('main.html')
+    list_images = ['kgs02.png', 'kgs06.png', 'kmk04.png', 'knt03.png']
+    return render_template('main.html', list_images=list_images)
+
+
+@app.route('/catalog', methods=['GET'])
+def catalog():
+    css = url_for('static', filename='css/all.css')
+    list_images = [file for file in os.listdir('static/img/catalog')]
+    return render_template('catalog.html', list_images=list_images)
 
 
 if __name__ == '__main__':
